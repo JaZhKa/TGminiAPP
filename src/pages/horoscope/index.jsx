cba1b1a72462f68633cdaec78507665bebad7ffd
Yakov Lemenkov <yakoff4@gmail.com>
2024-08-15 22:37:48 +0300

    add horoscope page

--- src/pages/horoscope/index.jsx
+++ src/pages/horoscope/index.jsx
@@ -1,22 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useGetHoroscopes } from './../../hooks/useGetHoroscope';
 
-const Horoscope = ({sign, lang}) => {
-  const {horoscope, getHoroscope} = useGetHoroscopes();
+const Horoscope = ({ sign, lang }) => {
+	const { horoscope, loading, getHoroscope } = useGetHoroscopes();
+	const [currentDate, setCurrentDate] = useState(new Date());
 
-  async function getData(sign, lang) {
-    await getHoroscope(sign, lang);
-  }
+	async function getData(sign, lang) {
+		await getHoroscope(sign, lang);
+	}
 
-  useEffect(() => {
-    getData(sign, lang)
-  }, []);
+	useEffect(() => {
+		getData(sign, lang);
+	}, []);
 
 	return (
-    <div className='horoscope__main'>
-      {/* {horoscope.horoscope} */}
-    </div>
-  );
+			<div className="horoscope__main">
+				<p className='horoscope__date'>{currentDate.toLocaleDateString(lang, {day: 'numeric', month: 'long'})}</p>
+				{loading ? <p className='horoscope__text'>Loading...</p> : <p className='horoscope__text'>{horoscope?.horoscope}</p>}
+			</div>
+	);
 };
 
 export default Horoscope;
